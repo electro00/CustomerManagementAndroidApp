bf4ff97d8f3ca377237b2e72a6c4b1eabf3d07c7
Bálint István <h849689@stud.u-szeged.hu>
2021-05-29 14:09:00 +0200

    menubar complete + views

--- app/src/main/java/com/example/customermanagement/CustomerListActivity.java
+++ app/src/main/java/com/example/customermanagement/CustomerListActivity.java
@@ -1,12 +1,17 @@
 package com.example.customermanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,6 +28,7 @@ public class CustomerListActivity extends AppCompatActivity {
     private ManagingCustomerAdapter mAdapter;
 
     private int gridNumber = 1;
+    private boolean viewRow = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,5 +69,66 @@ public class CustomerListActivity extends AppCompatActivity {
         customersImageResource.recycle();
 
         mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.customer_list_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_bar);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(LOG_TAG, newText);
+                mAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_button:
+                Log.d(LOG_TAG, "Logout clicked");
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                return true;
+            case R.id.setting_button:
+                Log.d(LOG_TAG, "Setting clicked");
+                return true;
+            case R.id.view_selector:
+                Log.d(LOG_TAG, "ViewSelector clicked");
+                if (viewRow) {
+                    changeSpanCount(item, R.drawable.ic_grid_view, 1);
+                } else {
+                    changeSpanCount(item, R.drawable.ic_row_view, 2);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void changeSpanCount(MenuItem item, int drawableID, int spanCount) {
+        viewRow = !viewRow;
+        item.setIcon(drawableID);
+        GridLayoutManager layoutManager = (GridLayoutManager) mRecyclerView.getLayoutManager();
+        layoutManager.setSpanCount(spanCount);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 }
